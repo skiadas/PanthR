b3608de220e63debf6f3ed07d4d3c0ae91a9ea6b
R.J. Beitz <beitzr14@gmail.com>
2013-05-01 16:01:25 -0400

    allowed top add user to the database

--- source/libs/db.js
+++ source/libs/db.js
@@ -1,53 +1,56 @@
-
 var mongodb = require('mongodb');
-var server = new mongodb.Server('localhost', 27017, {auto_reconnect: true});
+var server = new mongodb.Server('localhost', 27017, {
+   auto_reconnect: true
+});
 
 
-//intitalize function
-//input: takes a database object
-//if known contects database object (ex. testing)
-//if it doesnt find calls function to initialize standard
-function init(dbObject)
-{
-	if(db.collection(dbObject).find() != true)
-	{
-		var db = new mongdb.Db('testingdb', server);
-		return db;
-	}
-	//standard function
-	//conects actual database
-	else
-	{
-		var db = new mongdb.Db('panthrdb', server);
-		return
-	}
+// intitalize function
+// input: takes a database object
+// if known contects database object (ex. testing)
+// if it doesnt find calls function to initialize standard
+function init(dbObject) {
+   if (typeof dbObject !== 'undefined') {
+      this.db = dbObject;
+   } else {
+      this.db = new mongodb.Db('panthrdb', server); // default, conects actual database
+   }
+   return this;
 }
 
-
-//method 
 // open database connection
-db.open(function(err, db)
-{
-  if(!err){
-	//access or create
-	db.collection('users', function(err,collection){
-	  //remove all docs
-	  collection.romove(null,{safe : true}, function(err, result){
-	    if(!err){
-	      var user = creatUser(object);
-	      
+function open() {
+   db.open(function (err, db) {
+      if (!err) {
+         // access or create
+         db.collection('users', function (err, collection) {
+            if (err) throw err;
+            if (collection) console.log('Were In!');
+         });
+      };
+   });
+   return this;
 }
 
-function createUser(user)
-{
-	db.collection('users').insert(user, 
-	  function(err, result) {    
-	    if (err) throw err;    
-	    if (result) console.log('Added!');
-	    
-	  });
+function createUser(user) {
+   this.db.open(function (err, db) {
+      if(err) throw err;
+      db.collection('users',function(err,collection){
+         collection.insert(user, function (err, result) {
+            if (err) throw err;
+            console.log('Added user!', user.email);
+            db.close();
+         });
+      });
+   });
+   //this.db.close();
+   return this;
 }
 
+module.exports = {
+   init: init,
+   createUser: createUser,
+   open: open
+};
 
-
+module.exports.init().createUser({email: 'b@a.com'});
 
