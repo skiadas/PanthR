8db0259bc03a218785c4d13812fe74688984a74b
R.J. Beitz <beitzr14@gmail.com>
2013-05-02 14:17:29 -0400

    JS beautifying code

--- source/libs/user.js
+++ source/libs/user.js
@@ -1,22 +1,24 @@
-var User = function (obj)
-{
-	for(i in obj)
-	  {
-	    if(obj.hasOwnProperty(i))
-	      {
-	      this[i] = obj[i];
-	      }
-	   }
-}	 
+var User = function (obj) {
+   for (i in obj) {
+      if (obj.hasOwnProperty(i)) {
+         this[i] = obj[i];
+      }
+   }
+}
 
 User.prototype = {
-  print: function() 
-    { return this.fname + ' ' + this.lname}
-    
+   print: function () {
+      return this.fname + ' ' + this.lname
+   }
+
 };
 
-var a = new User({_id:'beitzr14',fname:'john', lname:'doe'});
+var a = new User({
+   email: 'a1@a.com',
+   fname: 'john',
+   lname: 'doe'
+});
 console.log(a instanceof User);
-console.log(a.print()); 
+console.log(a.print());
 
 module.exports = User;
