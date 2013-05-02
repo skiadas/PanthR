d505ad67ef5aceb0b5a7dfe2fc086e455d2ebe1f
R.J. Beitz <beitzr14@gmail.com>
2013-05-02 12:43:19 -0400

    fixed function so that multiple users are not created just updated Fixes #33

--- source/libs/db.js
+++ source/libs/db.js
@@ -8,16 +8,22 @@ var server = new mongodb.Server('localhost', 27017, {
 // input: takes a database object
 // if known contects database object (ex. testing)
 // if it doesnt find calls function to initialize standard
+
 function init(dbObject) {
-   if (typeof dbObject !== 'undefined') {
-      this.db = dbObject;
-   } else {
-      this.db = new mongodb.Db('panthrdb', server); // default, conects actual database
+   if (this.db) {
+      this.db.close()
    }
+   var newdb = dbObject || new mongodb.Db('panthrdb', server);
+   var that = this;
+   newdb.open(function (err, db) {
+      if (err) throw err;
+      that.db = db
+   })
    return this;
 }
 
 // open database connection
+
 function open() {
    db.open(function (err, db) {
       if (!err) {
@@ -32,17 +38,28 @@ function open() {
 }
 
 function createUser(user) {
-   this.db.open(function (err, db) {
-      if(err) throw err;
-      db.collection('users',function(err,collection){
-         collection.insert(user, function (err, result) {
-            if (err) throw err;
-            console.log('Added user!', user.email);
-            db.close();
-         });
+   if (this.db) {
+      this.db.collection('users', function (err, collection) {
+         collection.findOne({
+            email: user.email
+         }, function (err, dbUser) {
+            if (!dbUser) {
+               collection.insert(user, function (err, result) {
+                  if (err) throw err;
+                  console.log('Added user!', user.email);
+               })
+            } else {
+               collection.update(dbUser, {
+                  $set: user
+               }, function (err, result) {
+                  console.log('Update User!', user.email);
+               })
+            }
+         })
       });
-   });
-   //this.db.close();
+   } else {
+      throw new Error('db not open');
+   }
    return this;
 }
 
@@ -52,5 +69,5 @@ module.exports = {
    open: open
 };
 
-module.exports.init().createUser({email: 'b@a.com'});
-
+//module.exports.init()
+//setTimeout(function() {module.exports.createUser({email: 'b@a.com', name: 'Ron'})},1000);
