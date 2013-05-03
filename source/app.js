4c2a37801e02b7d0da96d3ccd3be31841861d2d9
Haris Skiadas <cskiadas@gmail.com>
2013-05-03 15:55:56 -0400

    Set up basic Passport functionality.

--- source/app.js
+++ source/app.js
@@ -6,8 +6,12 @@
 var express = require('express')
   , routes = require('./routes')
   , map = require('./maproutecontroller')
+  , passport = require('passport')
+  , flash = require('connect-flash')
+  , LocalStrategy = require('passport-local').Strategy
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , User = require('./libs/user');
 
 var app = express();
 
@@ -21,6 +25,9 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,14 +36,48 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// AUTHORIZATION SETUP
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+  },
+  function(email, password, done) {
+    User.find(email, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.email);
+});
+passport.deserializeUser(function(email, done) {
+  User.find(email, function(err, user) {
+    done(err, user);
+  });
+});
+
+// ROUTES
 app.get('/', routes.index);
 app.get('/register', routes.register);
 app.get('/login', routes.login);
-app.post('/login', routes.authenticate);
-var prefixes = ['user'];
-prefixes.forEach(function(prefix) {
-    map.mapRoute(app, prefix);
-});
+app.post('/login', 
+    passport.authenticate('local', { successRedirect: '/user',
+                                     failureRedirect: '/login',
+                                     failureFlash: true })
+);
+app.get('/user', 
+    passport.authenticate('local', { successRedirect: '/user',
+                                     failureRedirect: '/login',
+                                     failureFlash: true })
+);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
