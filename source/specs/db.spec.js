0d6d772d8e5d1facd5cbda1334d023ea5c1f228f
Dung Pham <phamd1989@gmail.com>
2013-05-08 11:19:12 -0400

    complete testing for updateUser()

--- source/specs/db.spec.js
+++ source/specs/db.spec.js
@@ -17,7 +17,8 @@ describe("A database connection", function () {
     // making a spy on the callback function
     var testCallBack = null;
         
-    beforeEach(function () {        
+    beforeEach(function () {       
+        // specify  
         testCallBack = {
             callback: function(arg1, arg2) {                
             }
@@ -64,7 +65,7 @@ describe("A database connection", function () {
     
     // set up a spy funct   ion -> check if the function gets called and what arguments have been passed
     
-    it("Tracks that the spy was called and its numbers of calls", function() {
+    it("Tracks that the spy of the callback function was called", function() {
         // create my own function
         
         // turn it into a spy function
@@ -72,12 +73,9 @@ describe("A database connection", function () {
         // asynchronous test, wait until the callback function get called
         
         // ask if it has been called and what arguments it passes
-        
-     
+             
         runs(function() {
             flag = false;
-            console.log('BEING RUN');
-            //testCallBack.callback(null, )
             setTimeout(function() {
                 flag = true;
             }, 100);
@@ -95,10 +93,10 @@ describe("A database connection", function () {
     });        
     
     
- // check whether the createUser() function exists and actually creates a new user
+ // check whether the createUser() function exists and gets called
     it("should have a createUser() function", function() {
         var dbmod = require('../libs/db.js');
-        var user = {email: 'fjsakfksal'};        
+        var user = {email: 'phamd13'};        
         
         // findOne finds an existing user, ignores insert
         var collectionMockPos = {
@@ -124,6 +122,7 @@ describe("A database connection", function () {
             }
         };
         
+        // create db properties for dbmod in POS case
         var collectionMockForDBPos = {
             collection: function(arg1, arg2) {
                 expect(arg1).toEqual('users');
@@ -132,6 +131,7 @@ describe("A database connection", function () {
             }
         };
         
+        // create db properties for dbmod in NEG case
         var collectionMockForDBNeg = {
             collection: function(arg1, arg2) {
                 expect(arg1).toEqual('users');
@@ -140,29 +140,79 @@ describe("A database connection", function () {
             }
         };        
         
-        dbmod.db = collectionMockForDBPos;
-        
+        dbmod.db = collectionMockForDBPos;        
         // expect dbmod has a defined property named 'createUser' 
         expect(dbmod.createUser).toBeDefined();
         // expect dbmod has a function called 'createUser'
         expect(dbmod.createUser).toEqual(jasmine.any(Function));                         
-        
+        // spy on the collection method
         spyOn(dbmod.db, 'collection').andCallThrough();
+        // spyon the findOne()
         spyOn(collectionMockPos, 'findOne').andCallThrough();
+        // spyon the insert()
         spyOn(collectionMockPos, 'insert').andCallThrough();
+        // make a call to the createUser()
         dbmod.createUser(user);
+        // some tests to check whether functions have been called
         expect(dbmod.db.collection).toHaveBeenCalled();               
         expect(collectionMockPos.findOne).toHaveBeenCalled();        
         
+        // the same process, in this case insert() will be called
         dbmod.db = collectionMockForDBNeg;        
         spyOn(collectionMockNeg, 'insert').andCallThrough();
         dbmod.createUser(user);                        
         expect(collectionMockNeg.insert).toHaveBeenCalled();
-        
-        //expect(dbmod.db.collection).toHaveBeenCalled();
-        //var dbCollection = dbmod.db.collection(); 
-        // need two mock objects, one for db and one for collection
-        // then test all methods of these two mock objects
+                
     });
 
+
+    // check whether the updateUser() exists and gets called
+    it("should have a updateUser() function", function() {
+        var dbmod = require('../libs/db.js');
+        var user = {email: 'phamd13'}; 
+        var changes = 'changes';       
+        
+        // findOne finds an existing user, proceeds to update()
+        // don't need to worry about the NEG case since 
+        // methods get called here
+        var collectionMockPos = {
+            findOne: function (arg1, arg2) {
+                expect(arg1).toEqual({email:user.email});
+                expect(arg2).toEqual(jasmine.any(Function)); 
+                arg2(null, user);                                       
+            },
+            update: function(arg1, arg2, arg3){
+                expect(arg1).toEqual(user);  
+                expect(arg2).toEqual({$set: changes});                                           
+            }
+        };
+        
+        // create db properties for dbmod in POS case
+        var collectionMockForDBPos = {
+            collection: function(arg1, arg2) {
+                expect(arg1).toEqual('users');
+                expect(arg2).toEqual(jasmine.any(Function));
+                arg2(null, collectionMockPos);                                
+            }
+        };
+                
+        dbmod.db = collectionMockForDBPos;        
+        // expect dbmod has a defined property named 'updateUser' 
+        expect(dbmod.updateUser).toBeDefined();
+        // expect dbmod has a function called 'updateUser'
+        expect(dbmod.updateUser).toEqual(jasmine.any(Function));                         
+        // spy on the collection method
+        spyOn(dbmod.db, 'collection').andCallThrough();
+        // spyon the findOne()
+        spyOn(collectionMockPos, 'findOne').andCallThrough();
+        // spyon the insert()
+        spyOn(collectionMockPos, 'update').andCallThrough();
+        // make a call to the createUser()
+        dbmod.updateUser(user, changes);
+        // some tests to check whether functions have been called
+        expect(dbmod.db.collection).toHaveBeenCalled();               
+        expect(collectionMockPos.findOne).toHaveBeenCalled();        
+        expect(collectionMockPos.update).toHaveBeenCalled();                               
+    });
+    
 });
