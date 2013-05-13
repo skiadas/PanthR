dce09680b011a6eed06bc970620ebed9151d86ba
Dung Pham <phamd1989@gmail.com>
2013-05-13 17:19:56 -0400

    add PubSub for Structure class, modify structure.update()

--- source/libs/structure.js
+++ source/libs/structure.js
@@ -40,6 +40,7 @@ Structure.prototype = {
 var util = require('util');
 var events = require('events');
 var db = require('./db.js');
+var PubSub = require('./pubsub.js');
 
 var Helper = function(){
     
@@ -53,6 +54,41 @@ var Helper = function(){
 
     };
     
+    // PubSub messaging 
+    // create()
+    PubSub.subscribe("structure/create", function{
+        //it should publish if db.createStructure() gets called
+        PubSub.publish('db/create/structure', {});
+    });
+    PubSub.subscribe("db/structure/created", function{
+        //it should publish if a new structure is created
+        PubSub.publish('structure/created', {});
+    });
+
+    // remove()
+    PubSub.subscribe("structure/remove", function{
+        //it should publish if db.removeStructure() gets called
+        PubSub.publish('db/remove/structure', {});
+    });
+    PubSub.subscribe("db/structure/removed", function{
+        //it should publish if a structure is removed
+        PubSub.publish('structure/removed', {});
+    });
+
+    // update()
+    PubSub.subscribe("structure/update", function{
+        //it should publish only if structure/updated gets published
+        // the reason is the object is already updated, we just need 
+        // to listen to when the object is updated in the database
+        // Note: how to notify other objects that this object has been updated
+        PubSub.publish('structure/updated', {});
+        //PubSub.publish('db/update/structure', {});
+    });
+    /*PubSub.subscribe("db/structure/updated", function{
+        //it should publish if a structure is updated
+        PubSub.publish('structure/updated', {});
+    });*/
+
     events.EventEmitter.call(That);
     util.inherits(That, events.EventEmitter);
     
@@ -61,14 +97,19 @@ var Helper = function(){
             return this.type + ' ' + this.owner_id + ' ' + this.parent + ' ' + this.links;
         },
         create : function(callback) {
-            db.createStructure(this, callback);
+            db.createStructure(this, callback);            
             return this;
         },
         remove : function(callback) {
-            db.removeStructure(this, callback);
+            db.removeStructure(this, callback);            
             //return this;
         },
         update: function(changes, callback){
+            for (i in changes) {
+                if (changes.hasOwnProperty(i)) {
+                    this[i] = changes[i];
+                 }
+            }  
             db.updateStructure(this, changes, callback);
             return this;
         }
